fbdf480792fabf2e844d2e0ea1e8212a05c409c9
Jonathan <jonathan.e.m.bocker@gmail.com>
2015-02-08 14:25:58 +0100

    Omg fixat GUI

--- src/inlamningsuppgift1/DiscLibraryApp.java
+++ src/inlamningsuppgift1/DiscLibraryApp.java
@@ -1,9 +1,22 @@
 package inlamningsuppgift1;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 
 public class DiscLibraryApp {
 	public static void main(String[] args) {
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+		} catch (Exception e) {
+		    // If Nimbus is not available, you can set the GUI to another look and feel.
+		}
 		DiscRegister register = new DiscRegister();		
 		Controller controller = new Controller(register);
 		GUI gui = new GUI(controller);
