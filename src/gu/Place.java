913e0b533c83c7756ed7eb4f38db2af598c0bb1f
Rhygar <john_tengvall@hotmail.com>
2015-03-05 14:52:44 +0100

    Har lagt till area och invånare i Place.

--- src/gu/Place.java
+++ src/gu/Place.java
@@ -3,12 +3,16 @@ package gu;
 public class Place {
     private String name;
     private Position position;
+    private double area;
+    private int population;
 
-    public Place(String name, double longitude, double latitude) {
+    public Place(String name, double longitude, double latitude, double area, int population) {
         this.name = name;
         this.position = new Position(longitude, latitude);
+        this.area = area;
+        this.population = population;
     }
-
+   
     public String getName() {
         return name;
     }
@@ -16,8 +20,17 @@ public class Place {
     public Position getPosition() {
         return position;
     }
+    
 
-    public String toString() {
+    public double getArea() {
+		return area;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public String toString() {
         return name;
     }
 }
