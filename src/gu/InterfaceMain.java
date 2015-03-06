f0e9b506375eb2cf7e17af3abd0ebb27f648a332
Jonathan <jonathan.e.m.bocker@gmail.com>
2015-03-06 16:30:41 +0100

    Grafiska justeringar
    Det är buggar när man drar i fönstret fortfarande

--- src/gu/InterfaceMain.java
+++ src/gu/InterfaceMain.java
@@ -1,32 +1,104 @@
 package gu;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 
 public class InterfaceMain {
 	private MapView map;
+	private ButtonListener btnListener = new ButtonListener();
+	private KeyBoardListener keyListener = new KeyBoardListener();
+	private JFrame frame = new JFrame("Test window");
+	private JPanel mapPanel = new JPanel(), searchPanel = new JPanel(), buttonPanel = new JPanel();
+	private JTabbedPane mainTabPane = new JTabbedPane();
+	private JScrollPane mapScrollPane;
+	private JButton btnSearch = new JButton("Search"), btnGoBack = new JButton("Go Back");
+	private JTextField tfSearch = new JTextField();
+	private JList<Place> placesList = new JList<Place>();
 
-	public InterfaceMain(String imagePath, Position mapLeftUp, Position mapRightDown) {
-		JFrame frame = new JFrame("Test window");
-		JPanel mapPanel = new JPanel();
-		JTabbedPane tabPane = new JTabbedPane();
-
+	public InterfaceMain(String imagePath, Position mapLeftUp,
+			Position mapRightDown) {
+		
+		// The Map Panel
 		map = new MapView(imagePath, mapLeftUp.getLongitude(),
 				mapLeftUp.getLatitude(), mapRightDown.getLongitude(),
 				mapRightDown.getLatitude());
-		mapPanel.add(map);
-		tabPane.addTab("Map", mapPanel);
+		
+		mapScrollPane = new JScrollPane(map);
+		mapPanel.setLayout(new BoxLayout(mapPanel, BoxLayout.Y_AXIS));
+		mapPanel.add(mapScrollPane);
+		mapPanel.add(Box.createGlue());
+		mainTabPane.addTab("Route Search", mapPanel);
+		
+		// Button Panel
+		btnGoBack.setEnabled(false);
+		
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+		buttonPanel.add(btnGoBack);		
+		buttonPanel.add(btnSearch);
+		tfSearch.addKeyListener(keyListener);
+		buttonPanel.add(tfSearch);
+		
+		// The Search panel
+		searchPanel.setLayout(new BorderLayout());
+		searchPanel.add(buttonPanel, BorderLayout.NORTH);
+		searchPanel.add(placesList, BorderLayout.CENTER);
+		mainTabPane.addTab("City search", searchPanel);
+
+		// Main frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(tabPane);
+		frame.add(mainTabPane);
 		frame.pack();
 		frame.setVisible(true);
 	}
-	
-	public void showRoads(ArrayList<Road> roads){
+
+	public void showRoads(ArrayList<Road> roads) {
 		map.showRoads(roads);
 	}
 
+	private class ButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == btnSearch){
+				JOptionPane.showMessageDialog(frame, "Detta funkar inte ännu!");
+			}
+		}
+	}
+	public class KeyBoardListener implements KeyListener {
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if(e.getExtendedKeyCode() == KeyEvent.VK_ENTER){
+				JOptionPane.showMessageDialog(frame, "Detta funkar inte ännu!");
+			}
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}		
+	}
 }
--- src/gu/MainGU.java
+++ src/gu/MainGU.java
@@ -1,10 +1,24 @@
 package gu;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 public class MainGU {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
+				try {
+					for (LookAndFeelInfo info : UIManager
+							.getInstalledLookAndFeels()) {
+						if ("Nimbus".equals(info.getName())) {
+							UIManager.setLookAndFeel(info.getClassName());
+							break;
+						}
+					}
+				} catch (Exception e) {
+
+				}
+				
 				Position mapLeftUp = new Position(12.527, 56.346); 
 				Position mapRightDown = new Position(14.596, 55.324); 
 				GUController controller = new GUController("/res/skane.JPG", mapLeftUp, mapRightDown,
