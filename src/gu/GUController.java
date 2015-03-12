4d312d5d3cdf127e9d0031f40f6984119af9adac
Rhygar <john_tengvall@hotmail.com>
2015-03-12 14:30:36 +0100

    Kommentering av controller del 1

--- src/gu/GUController.java
+++ src/gu/GUController.java
@@ -9,20 +9,28 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
+/**
+ * 
+ * 
+ * @author
+ *
+ */
 public class GUController {
 	private InterfaceMain window;
 	private ArrayList<Place> placesList = new ArrayList<Place>();
 	private ArrayList<Road> roadsList = new ArrayList<Road>();
-	private ArrayList<Road> printList; 
+	private ArrayList<Road> printList;
 	private Graph<Place> graph = new Graph<Place>();
 	private HashMap<String, Place> placeHash = new HashMap<String, Place>();
 	private HashMap<String, Road> roadHash = new HashMap<String, Road>();
 	private SearchTree search;
 
 	public GUController(String imagePath, Position mapLeftUp,
-			Position mapRightDown, String placesPath, String roadsPath, String iconPath) {
+			Position mapRightDown, String placesPath, String roadsPath,
+			String iconPath) {
 
-		window = new InterfaceMain(imagePath, iconPath, mapLeftUp, mapRightDown, this);
+		window = new InterfaceMain(imagePath, iconPath, mapLeftUp,
+				mapRightDown, this);
 		loadFiles(roadsPath, placesPath);
 	}
 
@@ -41,8 +49,8 @@ public class GUController {
 
 	public void searchDepthFirst(String from, String to) {
 		if (graph.containsVertex(placeHash.get(from))) {
-			printList = convertEdgeToRoad(GraphSearch.depthFirstSearch(
-					graph, placeHash.get(from), placeHash.get(to)));
+			printList = convertEdgeToRoad(GraphSearch.depthFirstSearch(graph,
+					placeHash.get(from), placeHash.get(to)));
 			window.showRoads(printList);
 			window.printRoadInfo(printList);
 		}
@@ -50,8 +58,8 @@ public class GUController {
 
 	public void searchBreadthFirst(String from, String to) {
 		if (graph.containsVertex(placeHash.get(from))) {
-			printList = convertEdgeToRoad(GraphSearch.breadthFirstSearch(
-					graph, placeHash.get(from), placeHash.get(to)));
+			printList = convertEdgeToRoad(GraphSearch.breadthFirstSearch(graph,
+					placeHash.get(from), placeHash.get(to)));
 			window.showRoads(printList);
 			window.printRoadInfo(printList);
 		}
@@ -59,13 +67,17 @@ public class GUController {
 
 	public void searchDijkstra(String from, String to) {
 		if (graph.containsVertex(placeHash.get(from))) {
-			printList = convertEdgeToRoad(GraphSearch.dijkstraSearch(
-					graph, placeHash.get(from), placeHash.get(to)));
+			printList = convertEdgeToRoad(GraphSearch.dijkstraSearch(graph,
+					placeHash.get(from), placeHash.get(to)));
 			window.showRoads(printList);
 			window.printRoadInfo(printList);
 		}
 	}
-
+/**
+ * 
+ * @param roadsPath
+ * @param placesPath
+ */
 	public void loadFiles(String roadsPath, String placesPath) {
 		BufferedReader br = null;
 		File roadsFile = new File(this.getClass().getResource(roadsPath)
@@ -80,17 +92,22 @@ public class GUController {
 			String line;
 			Place tempPlace;
 			Road tempRoad;
-
+			//will only execute if line in text file is not empty
 			while ((line = br.readLine()) != null) {
+				//if line in text file starts with "/" it will not do anything
 				if (line.charAt(0) == '/') {
 				} else {
+					//Creates an String array 
 					String[] placeArray = line.split(" ");
+					//Puts data from text file in index order in tempPlace. E.g index 0 contains city name
 					tempPlace = new Place(placeArray[0],
 							Double.parseDouble(placeArray[2]),
 							Double.parseDouble(placeArray[1]),
 							Double.parseDouble(placeArray[3]),
 							Integer.parseInt(placeArray[4]));
+					//adds tempPlace in ArrayList placesList
 					placesList.add(tempPlace);
+					//first index(city name) is key in HashMap and it associate with rest of the array
 					placeHash.put(placeArray[0], tempPlace);
 				}
 			}
@@ -126,6 +143,11 @@ public class GUController {
 		makeGraph(placesList, roadsList);
 	}
 
+	/**
+	 * 
+	 * @param path
+	 * @return tempRoadsList
+	 */
 	private ArrayList<Road> convertEdgeToRoad(ArrayList<Edge<Place>> path) {
 		ArrayList<Road> tempRoadsList = new ArrayList<Road>();
 		for (Edge<Place> road : path) {
@@ -135,6 +157,11 @@ public class GUController {
 		return tempRoadsList;
 	}
 
+	/**
+	 * 
+	 * @param places
+	 * @param roads
+	 */
 	private void makeGraph(ArrayList<Place> places, ArrayList<Road> roads) {
 		for (Place place : places) {
 			graph.addVertex(place);
@@ -144,5 +171,7 @@ public class GUController {
 			graph.addEdge(placeHash.get(temp.getFrom()),
 					placeHash.get(temp.getTo()), temp.getCost());
 		}
+
 	}
+
 }
